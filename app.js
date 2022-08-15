ec4f9052eaba343389feadcf97aadf474d915b79
Ogilboot <callum_ogilvy@hotmail.co.uk>
2022-08-15 10:35:43 +0000

    feat: movement and remove functions added

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const grid = document.querySelector('.grid')
+let currentShooterIndex = 202
+let width = 15
 
 for (let i = 0; i < 225; i++) {
     const square = document.createElement('div')
@@ -13,8 +15,40 @@ const alienInvaders = [
     30,31,32,33,34,35,36,37,38,39
 ]
 
+
 function draw() {
     for (let i = 0; i < alienInvaders.length; i++) {
         squares[alienInvaders[i]].classList.add('invader')
     }
+}
+
+draw()
+
+function remove() {
+    for (let i = 0; i < alienInvaders.length; i++) {
+        squares[alienInvaders[i]].classList.remove('invader')
+    }
+}
+
+squares[currentShooterIndex].classList.add('shooter')
+
+function moveShooter(e) {
+    squares[currentShooterIndex].classList.remove('shooter')
+    switch(e.key) {
+        case 'ArrowLeft':
+            if(currentShooterIndex % width !== 0) currentShooterIndex -=1
+            break
+        case 'ArrowRight':
+            if(currentShooterIndex % width < width -1) currentShooterIndex +=1
+            break
+    }
+    squares[currentShooterIndex].classList.add('shooter')
+}
+document.addEventListener('keydown', moveShooter)
+
+function moveInvaders() {
+    const leftEdge = alienInvaders[0] & width === 0
+    const rightEdge = alienInvaders[alienInvaders.length - 1] & width === width -1
+
+
 }
